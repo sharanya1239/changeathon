18e6c682841651ad07c2df4a2a49fafad1a238d9
sharanya1239 <53859250+sharanya1239@users.noreply.github.com>
2019-11-16 03:03:08 +0530

    Add files via upload
    main

---
+++ main.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MAIN</title>
+    <style>
+ body{
+                background-image: url("img.jpg");
+                height:100%;
+                background-size:cover ;
+                background-color: #F3EBF6;
+        font-family: 'Ubuntu', sans-serif;
+            }
+            #one-google   {
+    
+    position: absolute;
+    top: 0;
+    transition: opacity 130ms;
+    width: 100%;
+
+}
+#one-google.show-element {
+    display: block;
+}
+h1{
+ font-family: 'Times New Roman', Times, serif;
+ text-align: center;
+ size:600px;
+ color:green;
+ 
+ height: 600px;
+ width: auto;
+}
+    </style>
+</head>
+<body>
+<h1>AGVENTURE</h1>  
+<div  id ="one-google" class="show-element">
+<a href="login.php" style="float:center;">Farmer login</a>
+
+        <a href="servicelogin.php" style="float:center;">Services login</a>
+
+<a href="consumerlogin.php" style="text-align:center;">Consumer login</a>
+        </div>
+
+
+</body>
+</html>
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
